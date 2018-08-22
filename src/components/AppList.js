700d3e716579cd4f58ba5bee64f0f12805a0ed7b
luandro <luandro@gmail.com>
2018-08-22 00:09:37 -0300

    added AppListNavbar with styles

--- src/components/AppList.js
+++ src/components/AppList.js
@@ -1,55 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Markdown from './Markdown'
 import AppListNavbar from './AppListNavbar'
 
-export default function AppList ({ t }) {
-  return (
-    <div className="wrapper">
-      <div className="container">
-      <Markdown
-        className="text"
-        t={t}
-        input={`## ENTER THE SCUTTLEVERSE`}
-        align="center"
-        largeWidth="250px"
-        margin="0 auto"
-      />
-      <img src="/static/ssb_diving_b.png" />
-      <AppListNavbar t={t} />
-      </div>
-      <style jsx>{`
-        .wrapper {
-          width: 100%;
-          padding-top: 180px;
-          text-align: center;
-        }
-        .container {
-          width: 90%;
-          margin: 0 auto;
-          display: flex;
-          flex-flow: column;
-          align-items: center;
-        }
-        img {
-          order: -1;
-        }
-        @media screen and (min-width: 1024px) {
+export default class AppList extends Component {
+  state = {
+    tab: 0,
+  }
+
+  changeTab = (index) => {
+    this.setState({
+      tab: index,
+    })
+  }
+
+  render () {
+    const { t } = this.props
+    const { tab } = this.state
+    return (
+      <div className="wrapper">
+        <div className="container">
+          <div className="titleContainer">
+            <Markdown
+              t={t}
+              input={`## ENTER THE SCUTTLEVERSE`}
+              align="center"
+              largeWidth="250px"
+              margin="0 auto"
+            />
+            <img src="/static/ssb_diving_b.png" />
+          </div>
+          <AppListNavbar t={t} onClick={this.changeTab} tab={tab} />
+        </div>
+        <style jsx>{`
+          .wrapper {
+            width: 100%;
+            padding-top: 180px;
+            text-align: center;
+          }
           .container {
-            display: inherit;
+            width: 90%;
+            margin: 0 auto;
+          }
+          .titleContainer {
+            display: flex;
+            flex-flow: column;
+            align-items: center;
+            padding-bottom: 150px;
           }
           img {
-            float: right;
-            position: relative;
-            top: -100px;
-            width: 30%;
+            order: -1;
           }
-        }
-        @media screen and (min-width: 1280px) {
-          img {
-           width: auto;
+          @media screen and (min-width: 1024px) {
+            .titleContainer {
+              display: inherit;
+            }
+            img {
+              float: right;
+              position: relative;
+              top: -100px;
+              width: 30%;
+            }
           }
-        }
-      `}</style>
-    </div>
-  )
+          @media screen and (min-width: 1280px) {
+            img {
+             width: auto;
+            }
+          }
+        `}</style>
+      </div>
+    )
+  }
 }
--- src/components/AppListNavbar.js
+++ src/components/AppListNavbar.js
@@ -1,19 +1,63 @@
 import React from 'react'
+import c from '../utils/colors'
+import TabIcons from './TabIcons'
+const tabs = [
+  {
+    img: 'desktop',
+    name: 'Desktop',
+  },
+  {
+    img: 'mobile',
+    name: 'Mobile',
+  },
+  {
+    img: 'cli',
+    name: 'Command Line',
+  },
+]
 
-export default function App ({ t }) {
+export default function App ({ t, onClick, tab }) {
   return (
     <div className="wrapper">
       <div className="container">
-        <p>{t('common:pureComponent')}</p>
+        {tabs.map((item, index) => (
+          <div key={index} className="navItem" onClick={() => onClick(index)}>
+            <TabIcons fill={(tab === index) ? c.primary : null} img={item.img} />
+            <a className={(tab === index) ? 'selected' : null}>{item.name}</a>
+          </div>
+        ))}
       </div>
+      <hr />
       <style jsx>{`
         .wrapper {
           width: 100%;
-          height: 600px;
         }
         .container {
           width: 90%;
           margin: 0 auto;
+          display: flex;
+          align-items: center;
+          align-content: center;
+          justify-content: space-around;
+        }
+        .navItem {
+          display: flex;
+          flex-flow: column;
+          align-items: center;
+          cursor: pointer;
+        }
+        a {
+          font-size: 1.2em;
+          padding-top: 15px;
+        }
+        svg > * {
+          fill: red;
+        }
+        .svg {
+          fill: red;
+        }
+        .selected {
+          color: ${c.primary};
         }
         @media (min-width: 1024px) {
           .container {
