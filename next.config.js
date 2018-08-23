9dbfaab593f96c4600226e0352706de88f04eef6
luandro <luandro@gmail.com>
2018-08-22 21:27:29 -0300

    added next.config for building index page

--- next.config.js
+++ next.config.js
@@ -3,5 +3,10 @@ const withOffline = require('next-offline')
 module.exports = withOffline({
   webpack(config, options) {
     return config
+  },
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' }
+    }
   }
 })
